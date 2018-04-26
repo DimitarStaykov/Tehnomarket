1f414c18fa966c94b7142a0aba4619c0a398a07b
DimitarStaykov <32131915+DimitarStaykov@users.noreply.github.com>
2018-04-26 15:31:33 +0300

    basic inputs for database

---
+++ products for database.sql
@@ -0,0 +1,37 @@
+INSERT INTO products
+(name,brand,price,categories_id)
+VALUES
+("Телевизор SAMSUNG 1","samsung",1000,7),
+("Телевизор SAMSUNG 2","samsung",2000,7),
+("Телевизор SAMSUNG 3","samsung",3000,7),
+("Телевизор SAMSUNG 4","samsung",4000,7),
+("Телевизор SAMSUNG 5","samsung",5000,7),
+("Телевизор SAMSUNG 6","samsung",7000,7),
+("Телевизор LG 1","lg",1000,7),
+("Телевизор LG 1","lg",2000,7),
+("Телевизор LG 1","lg",3000,7),
+("Телевизор LG 1","lg",4000,7);
+
+INSERT INTO products
+(name,brand,price,categories)
+VALUES
+("ЛАПТОП DELL 1","dell",1999,16),
+("ЛАПТОП DELL 2","dell",2999,16),
+("ЛАПТОП DELL 3","dell",3999,16),
+("ЛАПТОП DELL 4","dell",4999,16),
+("ЛАПТОП DELL 5","dell",999,16),
+("ЛАПТОП DELL 6","dell",6999,16),
+("ЛАПТОП HP 1","hp",1989,16),
+("ЛАПТОП HP 2","hp",2989,16),
+("ЛАПТОП HP 3","hp",3989,16),
+("ЛАПТОП HP 4","hp",4989,16);
+
+INSERT INTO products
+(name,brand,price,categories)
+VALUES
+("Мобилен Телефон HUAWEI 1","huawei",199,22),
+("Мобилен Телефон HUAWEI 2","huawei",299,22),
+("Мобилен Телефон HUAWEI 3","huawei",399,22),
+("Мобилен Телефон HUAWEI 4","huawei",499,22),
+("Мобилен Телефон HUAWEI 5","huawei",599,22),
+("Мобилен Телефон HUAWEI 6","huawei",699,22);
